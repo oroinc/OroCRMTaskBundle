1fd37f6f5837731939c1c7b85040754d0947c7d6
Nikolay Imshenitsky <mimshenytskyi@magecore.com>
2014-08-18 17:20:24 +0300

    OEE-41-update: Check organization relation in case we update app

---
+++ Migrations/Data/ORM/UpdateTaskWithOrganization.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OroCRM\Bundle\TaskBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\OrganizationBundle\Migrations\Data\ORM\UpdateWithOrganization;
+
+class UpdateTaskWithOrganization extends UpdateWithOrganization
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->update($manager, 'OroCRMTaskBundle:Task');
+    }
+}
