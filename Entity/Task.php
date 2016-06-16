93ac970d53d72657bf9b3d2e1712cad807c851f3
Mykhailo Kudelia <mkudelia@magecore.com>
2016-06-16 18:13:53 +0300

    BAP-10486: Refresh Role management UI - add category item to entities config

--- Entity/Task.php
+++ Entity/Task.php
@@ -35,7 +35,8 @@ use OroCRM\Bundle\TaskBundle\Model\ExtendTask;
  *      routeView="orocrm_task_view",
  *      defaultValues={
  *          "entity"={
- *              "icon"="icon-tasks"
+ *              "icon"="icon-tasks",
+ *              "category"="Task"
  *          },
  *          "ownership"={
  *              "owner_type"="USER",
