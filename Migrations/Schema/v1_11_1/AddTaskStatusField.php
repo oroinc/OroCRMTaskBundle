42b9a25f237bb2996b908e341811d4bce99cbc2e
Mykhailo Sulyma <msulima@magecore.com>
2016-09-12 19:38:01 +0300

    CRM-6223: Broken Tasks functionality in 1.12 OroCRM  - fixed migration

--- Migrations/Schema/v1_11_1/AddTaskStatusField.php
+++ Migrations/Schema/v1_11_1/AddTaskStatusField.php
@@ -29,6 +29,11 @@ class AddTaskStatusField implements Migration, ExtendExtensionAwareInterface
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+        $table = $schema->getTable('orocrm_task');
+        if ($table->hasColumn('status_id')) {
+            return;
+        }
+
         static::addTaskStatusField($schema, $this->extendExtension);
         static::addEnumValues($queries, $this->extendExtension);
 
