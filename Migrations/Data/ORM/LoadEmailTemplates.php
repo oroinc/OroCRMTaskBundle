4bef482fe7e7a33b5c5cb57e6401b8ff2f5fe04a
Michael Bessolov <mbessolov@users.noreply.github.com>
2020-04-30 09:18:07 -0700

    BAP-19831: Class namespaces not compliant with PSR-4 cause composer warnings (#27574)
    - fixed namespaces of migrations
    - fixed ORM CAST function mock
    - fixed a typo in the method name
    - added functionality to rename data fixtures
    - added documentation about rename data fixtures
    
    Co-authored-by: Andrey Sevastianov <asevastianov@oroinc.com>
    Co-authored-by: Kateryna Isakova <kisakova@users.noreply.github.com>

--- Migrations/Data/ORM/LoadEmailTemplates.php
+++ Migrations/Data/ORM/LoadEmailTemplates.php
@@ -3,41 +3,31 @@
 namespace Oro\Bundle\TaskBundle\Migrations\Data\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-
 use Oro\Bundle\EmailBundle\Migrations\Data\ORM\AbstractEmailFixture;
-use Oro\Bundle\MigrationBundle\Entity\Repository\DataFixtureRepository;
+use Oro\Bundle\MigrationBundle\Fixture\RenamedFixtureInterface;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-
-class LoadEmailTemplates extends AbstractEmailFixture
+/**
+ * Loading data for email templates
+ */
+class LoadEmailTemplates extends AbstractEmailFixture implements RenamedFixtureInterface
 {
     /**
      * {@inheritDoc}
      */
+    public function getPreviousClassNames(): array
+    {
+        return [
+            'Oro\\Bundle\\ReminderBundle\\Migrations\\Data\\ORM\\LoadEmailTemplates',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function load(ObjectManager $manager)
     {
-        // this migration was already loaded in previous versions, but in OroReminderBundle,
-        // so it will be loaded again because of namespace change
-
-        /** @var DataFixtureRepository $repo */
-        $repo = $manager->getRepository('OroMigrationBundle:DataFixture');
-
-        $isFixtureExists = $repo->isDataFixtureExists(
-            'm.className = ?0',
-            ['Oro\Bundle\ReminderBundle\Migrations\Data\ORM\LoadEmailTemplates']
-        );
-        if ($isFixtureExists) {
-            $repo->updateDataFixutreHistory(
-                [
-                    'm.className' => "'Oro\Bundle\TaskBundle\Migrations\Data\ORM\LoadEmailTemplates'",
-                ],
-                'm.className = ?0',
-                ['Oro\Bundle\ReminderBundle\Migrations\Data\ORM\LoadEmailTemplates']
-            );
-        } else {
-            parent::load($manager);
-        }
+        // Prevent BC break warning
+        parent::load($manager);
     }
 
     /**
