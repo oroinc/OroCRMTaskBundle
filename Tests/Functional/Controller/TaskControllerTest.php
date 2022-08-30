423601f47d5a7269bf2d1bd83f176d9e613dcb06
Nikita Makarov <mesaverde228@gmail.com>
2022-08-30 23:11:35 +0200

    BAP-21492: Task management buttons are not shown on Task information popup on Calendar widget (#33598)
    - fixed template path resolving for legacy widgets
    - updated phpunit test
    - Updated regexp to determine the template reference parts
    - Added functional test to ensure that widget actions be presented

--- Tests/Functional/Controller/TaskControllerTest.php
+++ Tests/Functional/Controller/TaskControllerTest.php
@@ -48,13 +48,40 @@ class TaskControllerTest extends WebTestCase
             $this->getUrl('oro_task_widget_info', ['id' => $this->getTask()->getId()])
         );
         $response = $this->client->getResponse();
+        self::assertResponseStatusCodeEquals($response, Response::HTTP_OK);
 
         /** Assert by prepared field */
+        $content = $response->getContent();
+        self::assertStringContainsString('Meet James', $content);
+        self::assertStringContainsString('Meet James in the office', $content);
+        self::assertStringContainsString('Normal', $content);
+        self::assertStringContainsString('John Doe', $content);
+
+        /** Assert widget actions are not exist, because not defined in template*/
+        self::assertStringNotContainsString('Delete', $content);
+        self::assertStringNotContainsString('View Task', $content);
+        self::assertStringNotContainsString('Edit Task', $content);
+    }
+
+    public function testInfoActionForCalendar()
+    {
+        /** Return task by id */
+        $this->client->request(
+            Request::METHOD_GET,
+            $this->getUrl('oro_task_widget_info', [
+                'id' => $this->getTask()->getId(),
+                '_widgetContainer'=>'Calendar',
+                '_wid'=> 'f536db4e-9fe3-4720-85f9-e5ffc4ceb006'
+            ])
+        );
+        $response = $this->client->getResponse();
         self::assertResponseStatusCodeEquals($response, Response::HTTP_OK);
-        self::assertStringContainsString('Meet James', $response->getContent());
-        self::assertStringContainsString('Meet James in the office', $response->getContent());
-        self::assertStringContainsString('Normal', $response->getContent());
-        self::assertStringContainsString('John Doe', $response->getContent());
+
+        /** Assert widget actions are exist */
+        $content = $response->getContent();
+        self::assertStringContainsString('Delete', $content);
+        self::assertStringContainsString('View Task', $content);
+        self::assertStringContainsString('Edit Task', $content);
     }
 
     public function testActivityAction()
