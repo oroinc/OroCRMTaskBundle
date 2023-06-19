86f368c1a4e65784c44fa12d1fb69ddee60ef1d0
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-06-19 11:14:07 +0300

    BAP-21019: Switch to PSR-12 coding standard except explicit constants visibility requirement (#35870)
    - used PSR-12 except required visibility for constants
    - updated php-cs-fixer config
    - added doc block comments for changed classes
    - updated php-semver-checker

--- Tests/Unit/Placeholder/PlaceholderFilterTest.php
+++ Tests/Unit/Placeholder/PlaceholderFilterTest.php
@@ -10,13 +10,13 @@ class PlaceholderFilterTest extends \PHPUnit\Framework\TestCase
     public function testIsCalendarTasksVisibleWithoutEnabledMyTasks()
     {
         $placeholder = new PlaceholderFilter(false);
-        self::assertFalse($placeholder->isCalendarTasksVisible(new \stdClass));
+        self::assertFalse($placeholder->isCalendarTasksVisible(new \stdClass()));
     }
 
     public function testIsCalendarTasksVisibleWithEnabledMyTasksWrongObject()
     {
         $placeholder = new PlaceholderFilter(true);
-        self::assertFalse($placeholder->isCalendarTasksVisible(new \stdClass));
+        self::assertFalse($placeholder->isCalendarTasksVisible(new \stdClass()));
     }
 
     public function testIsCalendarTasksVisibleWithEnabledMyTasks()
