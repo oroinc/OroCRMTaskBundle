5e7a828c8b9aa784f16ba1d670e190fa1c2b9afc
Vova Soroka <vsoroka@oroinc.com>
2023-12-31 18:11:42 +0200

    BAP-22380: Create a migration query to simplify loading of enum values (#37259)
    - add Oro\Bundle\EntityExtendBundle\Migration\Query\InsertEnumValuesQuery
    - add Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareInterface and Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareTrait
    - remove public static methods from some migrations
    - refactor some installers

--- Migrations/Schema/v1_11_1/AddTaskStatusField.php
+++ Migrations/Schema/v1_11_1/AddTaskStatusField.php
@@ -4,10 +4,11 @@ namespace Oro\Bundle\TaskBundle\Migrations\Schema\v1_11_1;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigEntityValueQuery;
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareTrait;
 use Oro\Bundle\EntityExtendBundle\Migration\OroOptions;
+use Oro\Bundle\EntityExtendBundle\Migration\Query\EnumDataValue;
+use Oro\Bundle\EntityExtendBundle\Migration\Query\InsertEnumValuesQuery;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -16,33 +17,28 @@ class AddTaskStatusField implements Migration, ExtendExtensionAwareInterface
     use ExtendExtensionAwareTrait;
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function up(Schema $schema, QueryBag $queries)
+    public function up(Schema $schema, QueryBag $queries): void
     {
         $table = $schema->getTable('orocrm_task');
         if ($table->hasColumn('status_id')) {
             return;
         }
 
-        static::addTaskStatusField($schema, $this->extendExtension);
-        static::addEnumValues($queries, $this->extendExtension);
-
+        $this->addTaskStatusField($schema, $queries);
         $queries->addPostQuery(new UpdateTaskStatusQuery($this->extendExtension));
-
-        $queries->addQuery(
-            new UpdateEntityConfigEntityValueQuery(
-                'Oro\Bundle\TaskBundle\Entity\Task',
-                'workflow',
-                'show_step_in_grid',
-                false
-            )
-        );
+        $queries->addQuery(new UpdateEntityConfigEntityValueQuery(
+            'Oro\Bundle\TaskBundle\Entity\Task',
+            'workflow',
+            'show_step_in_grid',
+            false
+        ));
     }
 
-    public static function addTaskStatusField(Schema $schema, ExtendExtension $extendExtension)
+    private function addTaskStatusField(Schema $schema, QueryBag $queries): void
     {
-        $enumTable = $extendExtension->addEnumField(
+        $enumTable = $this->extendExtension->addEnumField(
             $schema,
             'orocrm_task',
             'status',
@@ -51,12 +47,12 @@ class AddTaskStatusField implements Migration, ExtendExtensionAwareInterface
 
         $options = new OroOptions();
         $options->set('enum', 'immutable_codes', ['open', 'in_progress', 'closed']);
-
         $enumTable->addOption(OroOptions::KEY, $options);
-    }
 
-    public static function addEnumValues(QueryBag $queries, ExtendExtension $extendExtension)
-    {
-        $queries->addPostQuery(new InsertTaskStatusesQuery($extendExtension));
+        $queries->addPostQuery(new InsertEnumValuesQuery($this->extendExtension, 'task_status', [
+            new EnumDataValue('open', 'Open', 1, true),
+            new EnumDataValue('in_progress', 'In Progress', 2),
+            new EnumDataValue('closed', 'Closed', 3)
+        ]));
     }
 }
